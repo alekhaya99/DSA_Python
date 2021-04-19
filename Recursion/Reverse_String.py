7836f586898e3bfbd7811a37afb55f203522a036
alekhaya99 <alekhaya.datta@gmail.com>
2021-04-19 22:41:14 +0800

    reverse string

--- Recursion/Reverse_String.py
+++ Recursion/Reverse_String.py
@@ -2,4 +2,9 @@ letter=input("Please Enter a String")
 length_letter=len(letter)
 
 def Reverse_string(letter):
-    Reverse_string_updated=""
+    if len(letter)==0:
+        return letter
+    else:
+        return Reverse_string(letter[1:])+letter[0]
+
+print(Reverse_string(letter))
